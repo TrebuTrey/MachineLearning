307a507e8a4e91b694997126881c3cf7476bc208
TrebuTrey <trey.pubins@gmail.com>
2023-01-02 09:11:12 -0700

    "Adding functionality."

--- pokemon/keyboard.py
+++ pokemon/keyboard.py
@@ -9,7 +9,7 @@ import pydirectinput as inp     #pydirect input is perfect for controlling commo
 class Controller():
     FILEPATH = os.path.join('D:', 'Emulator', 'RetroArch', 'retroarch.cfg') #make sure you use '/' and not '\' when designating your directory
     FILEPATH
-
+     
     KEYBIND = "input_player1_"
     aButton = 'a = '
     bButton = 'b = '
@@ -20,7 +20,9 @@ class Controller():
     leftButton = 'left = '
     rightButton = 'right = '
     fastForward = 'input_toggle_fast_forward = '
-    with open(FILEPATH, 'r') as infile, open('input1_1.txt', 'w') as outfile:
+    resetButton = 'input_reset = '
+    screenshotButton = 'input_screenshot = '
+    with open(FILEPATH, 'r') as infile, open('pokemon/input1_1.txt', 'w') as outfile: #outfile is visual confirmation that the right inputs were taken from the .cfg file
             for line in infile:
                 if KEYBIND in line:
                     # outfile.write(line.replace(KEYBIND, ""))
@@ -46,6 +48,14 @@ class Controller():
                     line = line.replace(fastForward, "")
                     line = line.replace('"', "")
                     fastForward = line
+                if resetButton in line:
+                    line = line.replace(resetButton, "")
+                    line = line.replace('"', "")
+                    resetButton = line
+                if screenshotButton in line:
+                    line = line.replace(screenshotButton, "")
+                    line = line.replace('"', "")
+                    screenshotButton = line
 
             outfile.write("a = " + aButton)
             outfile.write("b = " + bButton)
@@ -56,6 +66,8 @@ class Controller():
             outfile.write("left = " + leftButton)
             outfile.write("right = " + rightButton)
             outfile.write("ff = " + fastForward)
+            outfile.write("reset = " + resetButton)
+            outfile.write("screenshot = " + screenshotButton)
     
 
 def mouseControl(): #establish where your X and Y position of the emulator will be on your screen so the mouse click in OpenEmulator will be set properly
@@ -76,15 +88,18 @@ def openEmulator():
         gui.hotkey('ctrl', 'esc')
         gui.write('RetroArch')
         gui.press('Enter')
-        time.sleep(3)                   # make sure the system is opening up with proper time so the click can register in the focus window
-        gui.leftClick(x= 300, y= 300)
-        time.sleep(3)
-        inp.press('backspace')
+        time.sleep(1)                   # make sure the system is opening up with proper time so the click can register in the focus window
+        # gui.leftClick(x= 300, y= 300)
+        # time.sleep(3)
+        # inp.press('backspace')
         inp.press('right' ,presses=3)
-        inp.press('down')
         inp.press('Enter', presses=2)
         
+def reset(x):
+    inp.typewrite(x)
 
+def moveIn(x):
+    inp.typewrite(x)
 
 cont = Controller()
 a = cont.aButton
@@ -96,8 +111,19 @@ d = cont.downButton
 l = cont.leftButton
 r = cont.rightButton
 ff = cont.fastForward
+re = cont.resetButton
+sc = cont.screenshotButton
 openEmulator()
-time.sleep(5)
-inp.typewrite(a*100)
+inp.typewrite(ff)
+time.sleep(1)
+inp.typewrite(ff)
+for i in range(5):
+    for i in range(10):
+        moveIn(u)
+    time.sleep(1)
+    reset(re)
+    time.sleep(15)
+
+# inp.typewrite(a*100)
 # mouseControl()
 
