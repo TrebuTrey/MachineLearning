7f5c62274fcf3641350b892fcc5feedcdea61ad4
Tanner Pubins <t.pubins@icloud.com>
2023-01-02 20:51:28 -0700

    Overhaul and clean up the controller module.

--- pokemon/controller.py
+++ pokemon/controller.py
@@ -1,78 +1,136 @@
 import os
-import os.path
 import platform
-import sys
 import time
 
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-import pyautogui as gui         #pyautogui is great for hotkeys and utilizing special characters on the system level, but it cannot register output in the applications
-import pydirectinput as inp     #pydirect input is perfect for controlling common button presses with the emulator development
-FILEPATH = os.path.join('D:', 'Emulator', 'RetroArch', 'retroarch.cfg') #make sure you use '/' and not '\' when designating your directory
-SCREENPATH = os.path.join('D:', 'Emulator', 'RetroArch', 'screenshots') #give access to your screenshots library for comparisons
-PLAY_GAME = False
-class Controller():
-     
-    KEYBIND = "input_player1_"
-    aButton = 'a = '
-    bButton = 'b = '
-    startButton = 'start = '
-    selectButton = 'select = '
-    upButton = 'up = '
-    downButton = 'down = '
-    leftButton = 'left = '
-    rightButton = 'right = '
-    fastForward = 'input_toggle_fast_forward = '
-    resetButton = 'input_reset = '
-    screenshotButton = 'input_screenshot = '
-    with open(FILEPATH, 'r') as infile, open('input1_1.txt', 'w') as outfile: #outfile is visual confirmation that the right inputs were taken from the .cfg file
-            for line in infile:
-                if KEYBIND in line:
-                    # outfile.write(line.replace(KEYBIND, ""))
-                    line = line.replace(KEYBIND, "")
-                    line = line.replace('"', "")
-                    if aButton == line[0:len(aButton)]:
-                        aButton = line.replace(aButton, "")
-                    if bButton == line[0:len(bButton)]:
-                        bButton = line.replace(bButton, "")
-                    if startButton == line[0:len(startButton)]:
-                        startButton = line.replace(startButton, "")
-                    if selectButton == line[0:len(selectButton)]:
-                        selectButton = line.replace(selectButton, "")
-                    if upButton == line[0:len(upButton)]:
-                        upButton = line.replace(upButton, "")
-                    if downButton == line[0:len(downButton)]:
-                        downButton = line.replace(downButton, "")
-                    if leftButton == line[0:len(leftButton)]:
-                        leftButton = line.replace(leftButton, "")
-                    if rightButton == line[0:len(rightButton)]:
-                        rightButton = line.replace(rightButton, "")
-                if fastForward in line:
-                    line = line.replace(fastForward, "")
-                    line = line.replace('"', "")
-                    fastForward = line
-                if resetButton in line:
-                    line = line.replace(resetButton, "")
-                    line = line.replace('"', "")
-                    resetButton = line
-                if screenshotButton in line:
-                    line = line.replace(screenshotButton, "")
-                    line = line.replace('"', "")
-                    screenshotButton = line
+import pyautogui as gui           #pyautogui is great for hotkeys and utilizing special characters on the system level, but it cannot register output in the applications
+if platform.system() == "Windows":
+    import pydirectinput as inp   #pydirect input is perfect for controlling common button presses with the emulator development
 
-            outfile.write("a = " + aButton)
-            outfile.write("b = " + bButton)
-            outfile.write("start = " + startButton)
-            outfile.write("select = " + selectButton)
-            outfile.write("up = " + upButton)
-            outfile.write("down = " + downButton)
-            outfile.write("left = " + leftButton)
-            outfile.write("right = " + rightButton)
-            outfile.write("ff = " + fastForward)
-            outfile.write("reset = " + resetButton)
-            outfile.write("screenshot = " + screenshotButton)
+from config import (
+    EMULATOR_NAME, RETROARCH_APP_FP,
+    RETROARCH_DIR, RETROARCH_CFG_FP, RETROARCH_SCREENSHOTS_DIR
+)
+
+
+class EmulatorController():
+
+    def __init__(self, player_num: int = 1):
+        self.player_num = player_num
+        
+        # parse the config file
+        keybind = f"input_player{player_num}_"
+        a_btn_str = f"{keybind}a = "
+        b_btn_str = f"{keybind}b = "
+        start_btn_str = f"{keybind}start = "
+        select_btn_str = f"{keybind}select = "
+        up_btn_str = f"{keybind}up = "
+        down_btn_str = f"{keybind}down = "
+        left_btn_str = f"{keybind}left = "
+        right_btn_str = f"{keybind}right = "
+        fast_fwd_btn_str = "input_toggle_fast_forward = "
+        reset_btn_str = "input_reset = "
+        screenshot_btn_str = "input_screenshot = "
+
+        def _clean_line(line: str, sub_str: str) -> str:
+            return line.replace(sub_str, "").replace("\"", "").replace("\n", "")
+
+        with open(RETROARCH_CFG_FP, "r") as infile:
+            for line in infile:
+                if a_btn_str in line:
+                    self.a_btn = _clean_line(line, a_btn_str)
+                elif b_btn_str in line:
+                    self.b_btn = _clean_line(line, b_btn_str)
+                elif start_btn_str in line:
+                    self.start_btn = _clean_line(line, start_btn_str)
+                elif select_btn_str in line:
+                    self.select_btn = _clean_line(line, select_btn_str)
+                elif up_btn_str in line:
+                    self.up_btn = _clean_line(line, up_btn_str)
+                elif down_btn_str in line:
+                    self.down_btn = _clean_line(line, down_btn_str)
+                elif left_btn_str in line:
+                    self.left_btn = _clean_line(line, left_btn_str)
+                elif right_btn_str in line:
+                    self.right_btn = _clean_line(line, right_btn_str)
+                elif fast_fwd_btn_str in line:
+                    self.fast_fwd_btn = _clean_line(line, fast_fwd_btn_str)
+                elif reset_btn_str in line:
+                    self.reset_btn = _clean_line(line, reset_btn_str)
+                elif screenshot_btn_str in line:
+                    self.screenshot_btn = _clean_line(line, screenshot_btn_str)
+                else:
+                    pass
     
-def mouseControl(): #establish where your X and Y position of the emulator will be on your screen so the mouse click in OpenEmulator will be set properly
+    def press_a(self):
+        self._press_btn_emulator(self.a_btn)
+    
+    def press_b(self):
+        self._press_btn_emulator(self.b_btn)
+    
+    def press_start(self):
+        self._press_btn_emulator(self.start_btn)
+    
+    def press_select(self):
+        self._press_btn_emulator(self.select_btn)
+    
+    def move_up(self):
+        self._press_btn_emulator(self.up_btn)
+    
+    def move_down(self):
+        self._press_btn_emulator(self.down_btn)
+    
+    def move_left(self):
+        self._press_btn_emulator(self.left_btn)
+    
+    def move_right(self):
+        self._press_btn_emulator(self.right_btn)
+    
+    def fast_fwd(self):
+        self._press_btn_emulator(self.fast_fwd_btn)
+
+    def reset(self):
+        self._press_btn_emulator(self.reset_btn)
+    
+    def take_screenshot(self):
+        self._press_btn_emulator(self.screenshot_btn)
+    
+    def _press_btn_emulator(self, btn: str):
+        if platform.system() == "Darwin":
+            press_mac_key(btn)
+        elif platform.system() == "Windows":
+            inp.typewrite(btn)
+
+
+def press_mac_key(key: str):
+    print(f"pressing key: {key}")
+    gui.keyDown(key)
+    gui.keyUp(key)
+
+
+def open_emulator():
+    if platform.system() == "Darwin":
+        os.system(f"open {RETROARCH_APP_FP}")
+        time.sleep(5)
+        press_mac_key('left')
+        time.sleep(0.5)
+        press_mac_key('down')
+        press_mac_key('down')
+        time.sleep(1)
+        press_mac_key('right')
+        time.sleep(0.5)
+        press_mac_key('enter')
+        time.sleep(0.5)
+        press_mac_key('enter')
+    elif platform.system() == "Windows":
+        gui.hotkey("ctrl", "esc")
+        gui.write(EMULATOR_NAME)
+        gui.press("Enter")
+        time.sleep(1)                   # make sure the system is opening up with proper time so the click can register in the focus window
+        inp.press("right" ,presses=3)
+        inp.press("Enter", presses=2)
+
+
+def control_mouse(): #establish where your X and Y position of the emulator will be on your screen so the mouse click in OpenEmulator will be set properly
     try:
         while True:
             x, y = gui.position()
@@ -81,64 +139,3 @@ def mouseControl(): #establish where your X and Y position of the emulator will 
             print('\b' * len(positionStr), end='', flush=True)
     except KeyboardInterrupt:
         print('\n')        
-    
-def openEmulator():
-    compOS = platform.system()
-    
-    if compOS == 'Windows':
-        gui.hotkey('ctrl', 'esc')
-        gui.write('RetroArch')
-        gui.press('Enter')
-        time.sleep(1)                   # make sure the system is opening up with proper time so the click can register in the focus window
-        # gui.leftClick(x= 300, y= 300)
-        # time.sleep(3)
-        # inp.press('backspace')
-        inp.press('right' ,presses=3)
-        inp.press('Enter', presses=2)
-        
-def reset(x):
-    inp.typewrite(x)
-
-def moveIn(x):
-    inp.typewrite(x)
-
-def takeScreenshot(x):
-    inp.typewrite(x)
-    
-
-cont = Controller()
-a = cont.aButton
-b = cont.bButton
-st = cont.startButton
-se = cont.selectButton
-u = cont.upButton
-d = cont.downButton
-l = cont.leftButton
-r = cont.rightButton
-ff = cont.fastForward
-re = cont.resetButton
-sc = cont.screenshotButton
-if PLAY_GAME:
-    openEmulator()
-    inp.typewrite(ff)
-    time.sleep(1)
-    inp.typewrite(ff)
-    for i in range(5):
-        for i in range(10):
-            moveIn(u)
-            takeScreenshot(sc)
-        time.sleep(1)
-        reset(re)
-        time.sleep(15)
-
-# currentImage = os.listdir(SCREENPATH)[len(os.listdir(SCREENPATH)) - 1]
-# previousImage = os.listdir(SCREENPATH)[len(os.listdir(SCREENPATH)) - 2]
-
-# pic = open(SCREENPATH + '\\' + currentImage, 'r')
-
-# img = mpimg.imread(pic)
-# imgplot = plt.imshow(img)
-# plt.show()
-
-
-
