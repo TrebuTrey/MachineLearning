27da0b4e2465a6335f0a6dbff6a0d1e87ee93182
Tanner Pubins <t.pubins@icloud.com>
2023-01-05 12:59:00 -0700

    Rename compare module to image module.

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,14 +1,14 @@
 import config
-from compare import logger as compare_logger
 from controller import logger as controller_logger
 from dex import logger as dex_logger
 from emulator import logger as emulator_logger
+from image import logger as image_logger
 from main import logger as main_logger
 from helpers.log import get_logger
 
 
-compare_logger = get_logger(compare_logger.name, config.LOG_LEVEL)
 controller_logger = get_logger(controller_logger.name, config.LOG_LEVEL)
 dex_logger = get_logger(dex_logger.name, config.LOG_LEVEL)
 emulator_logger = get_logger(emulator_logger.name, config.LOG_LEVEL)
+image_logger = get_logger(image_logger.name, config.LOG_LEVEL)
 main_logger = get_logger(main_logger.name, config.LOG_LEVEL)
